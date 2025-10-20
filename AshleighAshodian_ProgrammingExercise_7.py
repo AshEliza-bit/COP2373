7aac199a0373c657b459541783e55fc7b5b05f05
Ashleigh Ashodian <datdoodlegirl@gmail.com>
2025-10-19 23:02:57 -0400

    version to submit

---
+++ AshleighAshodian_ProgrammingExercise_7.py
@@ -0,0 +1,21 @@
+import re
+
+def get_paragraph(): #prompts the user to enter a paragraph of their desired length
+    paragraph = input("Please enter a paragraph: ")
+    return paragraph #
+
+def count_sentences(paragraph): #keeps track of how many sentences there are
+    pat = r'[A-Z0-9].*?[.!?](?= [A-Z0-9]|$)' #the pattern from lesson 7.4 tweaked to also include sentences that start with numbers
+    sentences = re.findall(pat, paragraph)
+    return sentences #sentences is the list of sentences from the user's input
+
+def main():
+    paragraph = get_paragraph()
+    sentences = count_sentences(paragraph)
+
+    print("Individual sentences:")
+    for sentence in sentences:
+        print(f"--> {sentence.strip()}") #adds an arrow to each sentence
+    print(f"\nTotal number of sentences: {len(sentences)}") #print the number of sentences there are
+
+main()
