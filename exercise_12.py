b0bb8ee958caa95964a4e5ce8b93de65ab7581af
Ashleigh Ashodian <datdoodlegirl@gmail.com>
2025-11-23 13:06:55 -0500

    versions to submit

---
+++ exercise_12.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+data = np.genfromtxt(
+    "students.csv",          # your file name
+    delimiter=",",
+    dtype=None,              # let NumPy figure out types
+    encoding="utf-8",
+    names=True               # read the header row
+)
+
+print(data[:5])
